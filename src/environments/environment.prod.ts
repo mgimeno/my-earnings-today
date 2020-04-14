fd1fc207571359eb6ca64d349302cb89f456ffd0
Marcos Gimeno <marcos.gimeno2@city-holdings.co.uk>
2020-04-14 23:40:35 +0200

    use http instead of https
    Former-commit-id: 6fd2690ed010b623104d072165070d516406b76c

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appTitle: "My Earnings Today",
   appDescription: "Calculate how much you have already earned today and compare with others",
-  logoUrl: "https://www.myearningstoday.com/assets/logo.png",
+  logoUrl: "http://www.myearningstoday.com/assets/logo.png",
   localStoragePrefix: "my-earnings-today_",
   compareToolMaxPersons: 5,
   creatorProfileLink: "https://www.linkedin.com/in/marcosgimeno/"
