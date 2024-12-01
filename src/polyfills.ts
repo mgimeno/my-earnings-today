ebcf175ae95a19a393e7777c9a0f7da3f1a47f0b
mgimeno <mgimeno@gmail.com>
2024-12-01 19:35:54 +0100

    Update angular

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -225,7 +225,7 @@ if (language == "es") {
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 import { environment } from './environments/environment';
 
 
